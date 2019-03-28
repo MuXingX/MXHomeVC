16214d9ce05fef5c0bb7f74cda987584b19b1555
穆星星 <670148403@qq.com>
2019-03-28 16:10:14 +0800

    init project

---
+++ MXHomeVC/Classes/TRUNetworking.h
@@ -0,0 +1,16 @@
+//
+//  TRUNetworking.h
+//  FBSnapshotTestCase
+//
+//  Created by MuXingXing on 2019/3/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRUNetworking : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MXHomeVC/Classes/TRUNetworking.m
@@ -0,0 +1,12 @@
+//
+//  TRUNetworking.m
+//  FBSnapshotTestCase
+//
+//  Created by MuXingXing on 2019/3/28.
+//
+
+#import "TRUNetworking.h"
+
+@implementation TRUNetworking
+
+@end
